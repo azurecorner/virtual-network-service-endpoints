1951f4a800a326d6b25a14e5fc45b7bbbdba3135
Gora LEYE <leyegora@yahoo.fr>
2022-11-08 21:52:45 +0100

    upgrade to NTE7  fix

---
+++ dotnet/Solution/WebApi/Controllers/PostRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApi.Controllers;
+
+public class PostRequest
+{
+    public int UserId { get; set; }
+    public string Description { get; set; }
+    public IFormFile Image { get; set; }
+}
