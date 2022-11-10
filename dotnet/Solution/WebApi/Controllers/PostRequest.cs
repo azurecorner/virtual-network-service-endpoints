f719c9bc99dc4705ad8871ec6cc5326a4330e91c
Gora LEYE <leyegora@yahoo.fr>
2022-11-10 12:45:13 +0100

    fix build

--- dotnet/Solution/WebApi/Controllers/PostRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WebApi.Controllers;
-
-public class PostRequest
-{
-    public int UserId { get; set; }
-    public string Description { get; set; }
-    public IFormFile Image { get; set; }
-}
