23b0a3831b8163a7f572573c9f1e4d060c301e93
Gora LEYE <leyegora@yahoo.fr>
2022-11-13 14:26:55 +0100

    fix build

--- dotnet/WebApiSolution/WebMvc/Program.cs
+++ dotnet/WebApiSolution/WebMvc/Program.cs
@@ -4,24 +4,22 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
 // create an HttpClient used for accessing the API
 builder.Services.AddHttpClient("APIClient", client =>
 {
-    client.BaseAddress = new Uri(builder.Configuration["FileStoargeAPIRoot"]);
+    client.BaseAddress = new Uri(builder.Configuration["FileStoargeAPIRoot"] ?? string.Empty);
     client.DefaultRequestHeaders.Clear();
     client.DefaultRequestHeaders.Add(HeaderNames.Accept, "application/json");
 });
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
+//if (!app.Environment.IsDevelopment())
+//{
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
-}
+//}
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
